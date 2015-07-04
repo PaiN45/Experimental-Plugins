9439f610215f33747f7c44f44fb493b5b390de44
PaiN <giorgosemmpr@gmai.com>
2015-07-04 20:56:35 +0300

    Attempt to fix the plugin

--- MindFreeze.cs
+++ MindFreeze.cs
@@ -7,51 +7,64 @@ using Oxide.Core;
 
 namespace Oxide.Plugins
 {
-    [Info("Mind Freeze", "PaiN", "1.0.0", ResourceId = 0)]
+    [Info("Mind Freeze", "PaiN", "1.2.0", ResourceId = 1198)]
     [Description("Allows you to freeze players with a legit way.")]
-    public class MindFreeze : RustPlugin
+    class MindFreeze : RustPlugin
     {
-		public void Loaded()
-		{
-		if (!permission.PermissionExists("canmindfreeze")) permission.RegisterPermission("canmindfreeze", this);	
-			//LoadDefaultConfig(); Maybe gonna add this later.
-		}
-			
-			
-		private Timer timerrepeat;
 		
-		[ChatCommand("freeze")]
-		void cmdFreeze(BasePlayer player, string command, string[] args)
-		{
-			string steamId = player.userID.ToString();
-            if (permission.UserHasPermission(steamId, "canmindfreeze"))
+        void Loaded()
+        {
+        if (!permission.PermissionExists("canmindfreeze")) permission.RegisterPermission("canmindfreeze", this);
+            //LoadDefaultConfig(); Maybe gonna add this later.
+        }
+           
+       List<BasePlayer> frozenPlayers = new List<BasePlayer>();  
+		   
+        private Timer timerrepeat;
+       
+        [ChatCommand("freeze")]
+		void cmdFreeze(BasePlayer player, string cmd, string[] args)
+        {
+			var target = BasePlayer.Find(args[0]);
+			frozenPlayers.Add(target);
+			if (frozenPlayers.Contains(target)) 
 			{
-				var target = BasePlayer.Find(args[0]);
-				var position = target.transform.position;
-				var configPos = new Vector3(position.x, position.y , position.z);
+                var position = target.transform.position;
+                var configPos = new Vector3(position.x, position.y , position.z);
 
-				if(Vector3.Distance(target.transform.position, configPos) < 1)
-				{
-				var timerrepeat = timer.Repeat(1, 0, () => target.ClientRPCPlayer(null, target, "ForcePositionTo", new object[] { configPos }));
-				player.TransformChanged();
+                if(Vector3.Distance(target.transform.position, configPos) < 1)
+                {
+                var timerrepeat = timer.Repeat(1, 0, () => target.ClientRPCPlayer(null, target, "ForcePositionTo", new object[] { configPos }));
+                target.TransformChanged();
 
-				}
+                }
 			}
-			else
-				SendReply(player, "You do not have permission to use this command!");
-		
 		}
-		[ChatCommand("unfreeze")]//I will find another way to unfreeze but for now i cant think another way to do it.
-		void cmdUnFreeze(BasePlayer player, string command, string[] args)
+  
+		
+        [ChatCommand("unfreeze")]
+        void cmdUnFreeze(BasePlayer player, string command, string[] args)
+        {
+			var target = BasePlayer.Find(args[0]);
+			if (frozenPlayers.Contains(target)) 
+			{
+				frozenPlayers.Remove(target);
+			}
+					
+        }
+		
+		[ChatCommand("unfreezeall")]
+		void cmdUnFreezeAll(BasePlayer player, string command, string[] args)
 		{
-		timerrepeat.Destroy();	
+			frozenPlayers.Clear();		
 		}
-		
-		
-		public void Unloaded()
-		{
-			timerrepeat.Destroy();			
-		}
-	}
+       
+       
+        public void Unloaded()
+        {
+            timerrepeat.Destroy(); 
+			frozenPlayers.Clear();
+        }
+    }
 
 }
